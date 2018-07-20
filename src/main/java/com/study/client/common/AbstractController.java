ffd4ed9f70a26f1b54d5c32bc787f14bf324cf61
lyon <luying.xf@gmail.com>
2018-07-20 18:50:54 +0800

    更新

---
+++ src/main/java/com/study/client/common/AbstractController.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C) Skywares Information Technology, LTD. 
+ * All Rights Reserved.
+ *
+ * AbstractController.java created on Jul 20, 2018 4:07:00 PM by Lyon Lu 
+ */
+package com.study.client.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <pre>
+ * Description:
+ * 
+ * @author Lyon Lu
+ * @date Jul 20, 2018 4:07:00 PM
+ *
+ * </pre>
+ */
+public abstract class AbstractController
+{
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+}
